53e48140561a49e3e03c46b60993c05253099b29
DGOLD7788 <80936904545dgold@gmail.com>
2024-03-23 21:58:46 +0200

    add id to nav

--- src/main.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelector('.mobile-menu-open-btn').addEventListener('click', function () {
-  document.querySelector('.mob-menu').classList.toggle('is-open');
-});
-
-document.querySelector('.mob-menu-btn').addEventListener('click', function () {
-  document.querySelector('.mob-menu').classList.toggle('is-open');
-});
